3b19d5b6518aed5400bce72c580e898307842ae5
Kharol Chicaiza <kharol.chicaiza@epn.edu.ec>
2023-02-22 11:40:07 -0500

    fr-paths
    paths a las diferentes paginas dle banco

--- banco/src/app/app-routing.module.ts
+++ banco/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { CreateclienteComponent } from './components/createcliente/createcliente.component';
+import { CreateuserComponent } from './components/createuser/createuser.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
- 
+  {path:'inicio',component:HomeComponent},
+  {path:'crearUsuario',component:CreateuserComponent},
+  {path:'login',component:LoginComponent},
+  {path:'crearCliente',component:CreateclienteComponent},
+  {path:'contacto',component:ContactoComponent},
+  {path:'**',component:HomeComponent}
 ];
 
 @NgModule({
