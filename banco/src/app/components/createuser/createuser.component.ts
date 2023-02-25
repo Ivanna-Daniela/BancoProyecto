2be553484d78a02c47ae7cf5842c7199fa2e6cf2
Kharol Chicaiza <kharol.chicaiza@epn.edu.ec>
2023-02-25 00:19:49 -0500

    fr-errores user
    Indica si hay errores en los datos al ingresar los datos para el user

--- banco/src/app/components/createuser/createuser.component.ts
+++ banco/src/app/components/createuser/createuser.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Cliente } from 'src/app/models/cliente';
+import { CargarService } from 'src/app/services/cargar.service';
+import { ClienteService } from 'src/app/services/cliente.service';
+import { Global } from 'src/app/services/global';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-createuser',
   templateUrl: './createuser.component.html',
-  styleUrls: ['./createuser.component.css']
+  styleUrls: ['./createuser.component.css'],
+  providers:[ClienteService,CargarService]
 })
-export class CreateuserComponent {
+export class CreateuserComponent implements OnInit{
+  public numero:string;
+  public cliente:Cliente;
+  public url:string;
+  public status:string;
+  public idGuardado:string;
 
+  constructor(
+    private _clienteService:ClienteService,
+    private _cargarService:CargarService,
+    private _router:Router,
+    private _route:ActivatedRoute
+  ){
+    this.numero="000";
+    this.url=Global.url;
+    this.cliente=new Cliente("","","","",999999999,"","");
+    this.status="";
+    this.idGuardado="";
+  }
+
+
+  ngOnInit():void{
+
+  }
+
+  buscarCliente(form:NgForm){
+    this._clienteService.getClienteN(this.numero).subscribe(
+      response=>{
+        if(response.cliente){
+          
+            this.cliente=response.cliente;
+            this.status='success';
+            this.idGuardado=response.cliente._id;
+            form.reset();
+            console.log("si pasa");
+          
+        }else{
+          this.status='failed';
+        }
+      },
+      error=>{
+        console.log(<any>error);
+      }
+    );
+  }
 }
