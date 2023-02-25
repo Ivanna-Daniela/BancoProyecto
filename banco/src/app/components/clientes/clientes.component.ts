34f8d0693620803b76f38a3469f5b44a7a0a6a40
Ivanna-Daniela <ivanna.cevallos@epn.edu.ec>
2023-02-25 11:09:35 -0500

    Modificaciones createcuenta Kharo

--- banco/src/app/components/clientes/clientes.component.ts
+++ banco/src/app/components/clientes/clientes.component.ts
@@ -12,15 +12,19 @@ import { Global } from '../../services/global';
 export class ClientesComponent implements OnInit{
 public clientes:Cliente[];
 public url:string;
+public numero:string; // add a variable to store the client number
 
 constructor(
   private _clienteService:ClienteService
 ){
   this.url=Global.url;
   this.clientes=[];
+  this.numero = ''; // initialize the variable
+
 }
 ngOnInit(): void{
   this.getClientes();
+  this.searchCliente();
 }
 getClientes(){
   this._clienteService.getClientes().subscribe(
@@ -36,4 +40,19 @@ getClientes(){
     }
   ); 
 }
+
+searchCliente(){ // create a new method to search for a client
+  this._clienteService.getClienteN(this.numero).subscribe(
+    response=>{
+      if(response.cliente){
+        this.clientes = [response.cliente]; // put the found client in an array
+        console.log(this.clientes);
+      }
+    },
+    error=>{
+      console.log(<any>error);
+    }
+  ); 
+}
+
 }
--- banco/src/app/components/createcliente/createcliente.component.ts
+++ banco/src/app/components/createcliente/createcliente.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CargarService } from 'src/app/services/cargar.service';
+import { ClienteService } from 'src/app/services/cliente.service';
+import { Cliente } from '../../models/cliente';
+import { NgForm } from '@angular/forms';
+import { Global } from '../../services/global';
 
 @Component({
   selector: 'app-createcliente',
   templateUrl: './createcliente.component.html',
-  styleUrls: ['./createcliente.component.css']
+  styleUrls: ['./createcliente.component.css'],
+  providers: [ClienteService, CargarService]
 })
-export class CreateclienteComponent {
+export class CreateclienteComponent implements OnInit{
+  public cliente:Cliente;
+  public clienteGuardar:Cliente;
+  public url:string;
+  public status:string;
+  public idGuardado:string;
 
+  constructor(
+    private _clienteService:ClienteService
+  ){
+    this.url=Global.url;
+    this.cliente= new Cliente('','','','',1,'','');
+    this.clienteGuardar= new Cliente('','','','',1,'','');
+    this.status="";
+    this.idGuardado="";
+  }
+
+  ngOnInit(): void {
+  }
+  guardarCliente(form:NgForm) {
+    this._clienteService.guardarCliente(this.clienteGuardar).subscribe(
+      response => {
+        if(response.cliente) {
+          this.status = 'success';
+          this.idGuardado = response.id;
+          this.clienteGuardar = response.cliente;
+          form.reset();
+        } else {
+          this.status = 'error';
+        }
+      },
+      error => {
+        console.log(<any>error);
+        this.status = 'error';
+      }
+    );
+  }
 }
